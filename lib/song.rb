470b42da0c7d1830924567405d0a621321c3ed5c
Andy Maskiell <maskiella@gmail.com>
2017-12-29 19:16:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
     genre_count_hash = {}
     @@genres.each do |genre|
       if genre_count_hash.has_key?(genre)
-        genre_count_hash[genre] = (genre_count_hash[genre] + 1)
+        genre_count_hash[genre] += 1
       else
         genre_count_hash[genre] = 1
       end
